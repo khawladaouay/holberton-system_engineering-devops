3b7ee990160c09510544e0228bf4f1a8c407c440
khawladaouay <daouaykhawla782@gmail.com>
2022-08-13 00:14:56 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file {'/tmp/school':
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
+#create a file in /tmp.
+file { '/tmp/school':
+  path    => '/tmp/school',
+  owner   => www-data,
+  group   => www-data,
+  mode    => '0744',
+  content => 'I love Puppet'
 }
