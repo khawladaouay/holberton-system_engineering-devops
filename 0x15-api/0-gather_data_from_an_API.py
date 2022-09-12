0e91c50db83f338ef2232aa3cab96e823b41ecd0
khawladaouay <daouaykhawla782@gmail.com>
2022-09-12 23:16:24 +0100

    fix

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""returns information"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = ('https://jsonplaceholder.typicode.com')
+    user = get("{}/users/{}".format(url, id)).json()
+    target = get("{}/users/{}/todos".format(url, id)).json()
+    u_name = user.get('name')
+    done = 0
+    for i in target:
+        if i['completed']:
+            done += 1
+    print("Employee {} is done with tasks({}/{}):".format(u_name, done,
+                                                          len(target)))
+    for done in target:
+        if done.get('completed'):
+            print("\t {}".format(done.get('title')))
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""returns information."""
+from requests import get
+from sys import argv
+from json import dump
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user = get("{}/users/{}".format(url, id)).json()
+    target = get('{}/todos?userId={}'.format(url, id)).json()
+    u_name = user.get('username')
+    fmt = {}
+    fmt[id] = []
+    for i in target:
+        appends = {'task': i['title'], 'completed': i['completed'],
+                   'username': u_name}
+        fmt[id].append(appends)
+    file_format = '{}.json'.format(id)
+    with open(file_format, 'w') as result:
+        dump(fmt, result)
