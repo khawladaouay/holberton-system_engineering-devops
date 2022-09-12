ccd87bc19ddd6aa226bdaa6dece90bfc6a21c3d9
khawladaouay <daouaykhawla782@gmail.com>
2022-09-12 23:38:31 +0100

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,13 +8,20 @@ if __name__ == '__main__':
     user = get('{}/users'.format(url)).json()
     target = get('{}/todos'.format(url)).json()
     data = {}
-    for i in user:
-        data[i['id']] = []
-        for x in target:
-            f = {'username': i['username'], 'task': x['title'],
-                   'completed': x['completed']}
-            if x['userId'] == i['id']:
-                data[i['id']].append(f)
-    json = 'todo_all_employees.json'
-    with open(json, 'w') as result:
-        dump(data, result)
+    for u in user:
+        employeeName = u.get('username')
+        todo = get(
+            "{}/users/{}/todos".format(url, u.get('id'))).json()
+        row = []
+        for elem in todo:
+            if elem.get('userId') == u.get('id'):
+                dict = {
+                    "username": employeeName,
+                    "task": elem.get('title'),
+                    "completed": elem.get('completed')
+                }
+                row.append(dict)
+            data[u.get('id')] = row
+
+    with open('todo_all_employees.json', 'w', encoding="utf-8") as file:
+        file.write(dump(data))
