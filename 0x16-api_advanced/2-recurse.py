cf98b10be29df0c3352368beb2b4ee9472fa4966
khawladaouay <daouaykhawla782@gmail.com>
2022-09-28 09:37:43 +0100

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,16 +8,19 @@ def recurse(subreddit, hot_list=[], after=''):
     titles of all hot articles for a given subreddit."""
     if not subreddit:
         return None
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    req = requests.get("{}".format(url), headers={
-        'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
+    url = "https://www.reddit.com"
+    endpoint = "/r/{}/hot.json?limit=100&after={}".format(subreddit, after)
 
-    if req.status_code == 200:
-        children = req.json().get('data').get('children')
-        after = req.json().get('data').get('after')
+    request = requests.get("{}{}".format(url, endpoint),
+                           headers={'User-agent': 'Mozilla/5.0'},
+                           allow_redirects=False)
 
-        for i in children:
-            hot_list.append(i.get('data').get('title'))
+    if request.status_code == 200:
+        children = request.json().get('data').get('children')
+        after = request.json().get('data').get('after')
+
+        for elem in children:
+            hot_list.append(elem.get('data').get('title'))
 
         if after is not None:
             recurse(subreddit, hot_list, after)
