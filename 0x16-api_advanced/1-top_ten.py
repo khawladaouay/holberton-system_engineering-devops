3b3cc40babb93cf236703f0f304c30425cf11ca5
khawladaouay <daouaykhawla782@gmail.com>
2022-09-28 09:30:08 +0100

    tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     if not subreddit:
         print('None')
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    req = requests.get(f"{url}", headers={
+    req = requests.get("{}".format(url), headers={
         'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
     if req.status_code == 200:
         for i in req.json().get('data').get('children'):
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,8 +9,9 @@ def recurse(subreddit, hot_list=[], after=''):
     if not subreddit:
         return None
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    req = requests.get(f"{url}", headers={
+    req = requests.get("{}".format(url), headers={
         'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
+
     if req.status_code == 200:
         children = req.json().get('data').get('children')
         after = req.json().get('data').get('after')
